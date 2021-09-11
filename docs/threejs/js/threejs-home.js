95bf6bfa900c70ffb5c14998f05653cb889b9636
Cosimo Romagnoli <cosimo@Cosimos-MacBook-Pro.local>
2021-09-11 17:44:19 +0100

    Changed number of particles and added screen boundaries

--- docs/threejs/js/threejs-home.js
+++ docs/threejs/js/threejs-home.js
@@ -19,20 +19,26 @@ const light = new THREE.PointLight( 0xffffff, 2.0);
 light.position.set( 0, 0.5, 5);
 scene.add(light);
 
-let particleSystem, point, particles;
+let particleSystem, point, particles, particleCount;
+
+particleCount = 120;
 
 particleSystem = new THREE.Geometry();
-for (let i=0; i < 200; i++) 
+for (let i=0; i < particleCount; i++) 
 {
 	point = new THREE.Vector3(
-		THREE.MathUtils.randFloatSpread(1000),
-		THREE.MathUtils.randFloatSpread(1000),
+		THREE.MathUtils.randFloatSpread(w),
+		THREE.MathUtils.randFloatSpread(h),
 		THREE.MathUtils.randFloat(-500, 0)
 	);
 	
 	let dir = 1 - THREE.MathUtils.randInt(0, 1) * 2; //this should yield either 1 or -1.
 
-	point.velocity = THREE.MathUtils.randFloat(0.2, 0.4) * dir;
+	
+	point.velocity = new THREE.Vector3( THREE.MathUtils.randFloat(0.2, 0.4) * dir, 
+										THREE.MathUtils.randFloat(0.2, 0.4) * dir, 
+										THREE.MathUtils.randFloat(0.2, 0.4) * dir);
+
 	point.acceleration = 0.0;
 
 	particleSystem.vertices.push(point);
@@ -44,10 +50,80 @@ particles = new THREE.Points( particleSystem, material );
 
 scene.add(particles);
 
+//clock for animate
+var clock = new THREE.Clock();
+var speed = 2; //units a second
+var delta = 0;
+
 /*
 	reference
 	https://www.youtube.com/watch?v=Bed1z7f1EI4
 */
+
+//========================CUSTOM FUNCTION =======================//
+function updateParticleSystem()
+{
+	particleSystem.vertices.forEach(p => {
+		//p.velocity += p.acceleration;
+		p.x += p.velocity.x;
+		p.y += p.velocity.y;
+		p.z += p.velocity.z;
+
+		//circular movement particles
+		// let radius = 20;
+
+		// let position = new THREE.Vector3(0, 0, 0);
+		// position.x = radius * Math.cos(clock.getDelta());
+		// position.y = radius * Math.sin(clock.getDelta());
+
+		// p.x = w/2;
+		// p.y = h/2;
+		// p.z = 10;
+
+		//=======Keeping the point within the screen=============//
+		//If the position of the point is beyond the screen size we send it back.
+		//Getting point coordinate projected to screen
+		let vector = new THREE.Vector3( p.x, p.y, p.z);
+		vector.project(camera);
+
+		vector.x = ( vector.x + 1) * w / 2;
+		vector.y = - ( vector.y - 1) * h / 2;
+
+		if(vector.x > w || vector.x<0) 
+		{
+			p.velocity.x = -p.velocity.x;
+		}
+
+		if(vector.y > h || vector.y <0)
+		{
+			p.velocity.y = -p.velocity.y;
+		}
+
+		if(vector.z > 0 || vector.z < -500)
+		{
+			p.velocity.z = -p.velocity.z;
+		}
+
+	});
+
+	particleSystem.verticesNeedUpdate = true;
+	//particles.rotation.y += 0.002;
+}
+
+function particleMakeCircle()
+{
+	//choose a random particle
+	//let i = THREE.MathUtils.randInt(0, particleCount-1);
+	
+	//radius of circle
+	// let radius = 20;
+
+	// let position = new THREE.Vector3();
+	// position.x = radius * THREE.Math.cos(Date.now());
+	// position.z = radius * THREE.Math.sin(Date.now());
+
+
+}
 
 //========================EVENT==================================//
 
@@ -66,15 +142,14 @@ window.addEventListener('resize', onWindowResize);
 //Game logic
 var update = function()
 {
-	particleSystem.vertices.forEach(p => {
-		p.velocity += p.acceleration;
-		p.x += p.velocity;
-		p.y += p.velocity;
-		p.z += p.velocity;
-	});
+	updateParticleSystem();
 
-	particleSystem.verticesNeedUpdate = true;
-	//particles.rotation.y += 0.002;
+	//this appens sporadically; with a chance of 20% now;
+	let chance = THREE.MathUtils.randInt(0, 100);
+	if(chance<20)
+	{
+		particleMakeCircle();
+	}
 }
 
 //Draw scene
